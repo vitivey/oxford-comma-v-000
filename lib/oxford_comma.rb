3c5c62053284ded7dea0794cf025ba49af64b22c
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 01:08:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  def last_comma(array)
+    if array.size == 2
+      " and "
+    elsif array.size==1
+      ""
+    else
+      ", and "
+    end
+  end
+  array[0..-2].join(", ") + "#{last_comma(array)}" + array[-1]
 end
